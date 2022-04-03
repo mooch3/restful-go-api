eebe4c15eed4beac36b10006f8451435274d38f2
Derek Smith <dcsmith.saxophone@gmail.com>
2022-04-03 11:38:51 -0700

    sfinished a simple 3 route api

--- main.go
+++ main.go
@@ -18,6 +18,29 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func getAlbum(c *gin.Context) {
+	id := c.Param("id")
+
+	for _,a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "no album found"})
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+	// call BindJSON to bind the received JSON to the newAlbum variable
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "17", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
@@ -27,5 +50,8 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbum)
+	router.POST("/albums", postAlbums)
+
 	router.Run("localhost:8080")
 }
